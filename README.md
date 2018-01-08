bbe297580b2dcf7e090bc1f169c0cb4cbb35ebae
jeffscottlevine <22220798+jeffscottlevine@users.noreply.github.com>
2018-01-08 00:50:46 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Introduction
+
+This repo contains the files associated with the django polling tutorial that can be found at https://www.djangoproject.com/.  I decided to name this project, MyPollingCompany, hence the name "mpc."  I am currently writing this to operate udner the following platforms.
+
+1. Amazon Linux 1 with the Cloud9 IDE on its own EC2 instance.
+
+# Instructions for Amazon Linux 1 with the Cloud9 IDE on its own EC2 instance
+
+1.  git config --global user.name "your name"
+2.  git config --global user.email "your email"
+3.  git clone "the https URL from github"
+4.  ./amazon_linux1_cloud9_setup.sh
+5.  unalias python
+6.  source env/bin/activate
+7.  python manage.py createsuperuser
+8.  python manage.py runserver 0:8080 
+9.  browse to the appropriate URL:8080/admin
+10. set up any users, questions, and choices
+11. browse to the appropriate URL:8080/polls/
+12. play around with the polls
+
+# Notes
+
+1. The files may contain multiple versions of content.  I did this so I could keep track of what changed throughout the tutorial.
+
+2. I am usinmg MySQL for the backend.
+
+# Security Considerations:
+
+1. This is a demo and is not built with strong security in mind.
+2. The passwords are short and are generated with pwgen.
+3. Passwords are used on command lines - a strong no no!
+
+# Attribution:
+
+1. The background image is from Kelli Tungay and came from https://unsplash.com/photos/RAVdOqWXPvg.
---
+++ amazon_linux1_cloud9_setup.sh
@@ -0,0 +1,162 @@
+#!/bin/bash
+
+# This script sets up the environment for the mpc (MyPollingCompany) demo
+# on an AWS Cloud9 instance runnng Amazon Linux 1.
+#
+# The MyPollingCompany demo is an implementation of the Django tutorial
+# at http://www.djangoproject.com. I wrote this so I could play with
+# Django under AWS Cloud9.
+#
+# The script does the following:
+#
+# - Installs mysql-devel which is needed for Django
+# - Installs pwgen to generate passwords
+# - Starts mysqld
+# - Generates password for the MySQL root user
+# - Generates password for the MySQL mpc user
+# - Does some basic MySQL security configuration
+# - Sets up the mpc database
+# - Sets up a python3 virtual environment
+#
+# Security notes:
+#
+# - The set up is not strong and is only meant for a short lived demo.
+# - The quality of passwords and keys is not that strong.
+# - Passwords are used on comamand lines which is not advisable.
+#
+# Artifacts:
+#
+# MPC_DJANGO_ENV - file used to hold configuration info that
+# will be supplied to Django.
+#
+# MYSQL_ROOT_PASSWORD - file that holds the MySQL root user password
+# in case things get messed up.
+
+# The mpc configuration file
+MPC_DJANGO_ENV=./mpc.env
+
+# The file in which to store the MySQL root password in case of issues
+MYSQL_ROOT_PASSWORD_FILE=$HOME/MYSQL_ROOT_PASSWORD
+
+echo checking for Amazon Linux version 1...
+expr index `uname -r` amzn1
+if [ $? -eq 0 ]
+then
+    echo "The operating system is not Amazon Linux 1."
+    exit 1
+fi
+
+if [ ! -L $HOME/.c9 ]
+then
+    echo "This does not appear to be an AWS Cloud 9 EC2 instance."
+    exit 1
+fi
+
+echo "installing mysql-devel..."
+sudo yum install -y -q mysql-devel
+if [ $? -ne 0 ]
+then
+    echo "Unable to install mysql-devel."
+    exit 1
+fi
+sleep 1
+
+echo "installing pwgen..."
+sudo yum install -y -q pwgen
+if [ $? -ne 0 ]
+then
+    echo "Unable to install pwgen."
+    exit 1
+fi
+sleep 1
+
+echo "starting mysqld..."
+sudo service mysqld start
+if [ $? -ne 0 ]
+then
+    echo "Unable to start mysqld."
+    exit 1
+fi
+sleep 1
+
+echo "generating MySQL root password..."
+MYSQL_ROOT_PASSWORD=`pwgen 8 1`
+echo $MYSQL_ROOT_PASSWORD > $HOME/MYSQL_ROOT_PASSWORD
+sleep 1
+
+echo generating MySQL mpcuser password...
+MYSQL_MPCUSER_PASSWORD=`pwgen 8 1`
+sleep 1
+
+echo generating Django secret key...
+DJANGO_SECRET_KEY=`pwgen -s 50 1`
+sleep 1
+
+echo "setting MySQL root password..."
+mysql -u root \
+-e "UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD') WHERE User='root';"
+sleep 1
+
+echo "disabling MySQL remote root login..."
+mysql -u root \
+-e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+sleep 1
+
+echo "removing anonymous MySQL users..."
+mysql -u root \
+-e "DELETE FROM mysql.user WHERE User='';"
+sleep 1
+
+echo "dropping test MySQL database..."
+mysql -u root \
+-e "DROP DATABASE test;"
+sleep 1
+
+echo "flushing MySQL privileges..."
+mysql -u root \
+-e "FLUSH PRIVILEGES;"
+sleep 1
+
+echo "creating mpc database..."
+mysql -u root -p$MYSQL_ROOT_PASSWORD \
+-e "CREATE DATABASE mpc character set UTF8;"
+sleep 1
+
+echo "creating mpcuser..."
+mysql -u root -p$MYSQL_ROOT_PASSWORD \
+-e "CREATE USER mpcuser@localhost identified by '$MYSQL_MPCUSER_PASSWORD';"
+sleep 1
+
+echo "granting privileges on mpc database tabkes..."
+mysql -u root -p$MYSQL_ROOT_PASSWORD \
+-e "GRANT ALL PRIVILEGES on mpc.* to mpcuser@localhost;"
+sleep 1
+
+echo "creating mpc Django environment file..."
+echo "SECRET_KEY=$DJANGO_SECRET_KEY" > $MPC_DJANGO_ENV
+echo "DEBUG=True" >> $MPC_DJANGO_ENV
+echo "DB_NAME=mpc" >> $MPC_DJANGO_ENV
+echo "DB_USER=mpcuser" >> $MPC_DJANGO_ENV
+echo "DB_PASSWORD=$MYSQL_MPCUSER_PASSWORD" >> $MPC_DJANGO_ENV
+echo "DB_HOST=localhost" >> $MPC_DJANGO_ENV
+echo "TIME_ZONE=America/Los_Angeles" >> $MPC_DJANGO_ENV
+sleep 1
+
+echo setting up Python environment...
+unalias python 2>/dev/null
+virtualenv --python=/usr/bin/python3 env
+source env/bin/activate
+sleep 1
+
+echo install Python modules...
+pip install -r requirements.txt
+sleep 1
+
+echo setting up Django database tables...
+python manage.py migrate
+sleep 1
+
+echo
+echo Initial set up complete!
+echo
+echo Follow the instructions in README.md for what to do next!
